f7a9e140893f8f589c0348412254c1ed16d3d0cd
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2023-05-13 22:39:38 +1200

    started on adding the functionality for TASK 1, now creating a method to process the users input for <fingers> and <sum>

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -1,14 +1,56 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
+  private int roundNumber = 0;
 
   public Morra() {}
 
-  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {}
+  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
 
-  public void play() {}
+  }
+
+  public void play() {
+    // increment the round number for each time the user starts a new round.
+    roundNumber++;
+    String roundNumberString = convertIntToString(roundNumber);
+    
+    MessageCli.START_ROUND.printMessage(roundNumberString);
+    MessageCli.ASK_INPUT.printMessage();
+    
+    String input = Utils.scanner.nextLine();
+
+  }
 
   public void showStats() {}
+
+  public String convertIntToString(int number) {
+    return String.valueOf(number);
+  }
+
+  public int[] convertStringtoIntArray(String string) {
+    //first split the string by spaces
+
+    String[] splitString = string.split(" ");
+    
+    //check that it has exactly two elements in it only
+    //otherwise ask the user to try again
+
+    while (splitString.length != 2) {
+        MessageCli.INVALID_INPUT.printMessage();
+        String input = Utils.scanner.nextLine();
+    } 
+
+    //the first element of the split array should suit the restrictions for fingers sum
+    int fingers = Integer.valueOf(splitString[0]);
+    int sum = Integer.valueOf(splitString[1]);
+
+    return null;
+
+
+  }
 }
